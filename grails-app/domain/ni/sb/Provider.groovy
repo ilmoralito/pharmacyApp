666310c122c13ca57042d4a8def12e138caa5dd9
mario martinez <amakenadog@gmail.com>
2014-08-27 15:24:35 -0600

    Creada logica proveedor producto y agregados datos de prueba

--- grails-app/domain/ni/sb/Provider.groovy
+++ grails-app/domain/ni/sb/Provider.groovy
@@ -3,6 +3,7 @@ package ni.sb
 class Provider {
 	String name
   String address
+  SortedSet products
 
 	Date dateCreated
 	Date lastUpdated
@@ -10,7 +11,10 @@ class Provider {
   static constraints = {
     name blank:false, unique:true
     address blank:false, unique:true
+    products nullable:false
   }
+
+  static hasMany = [products:String]
 
   static mapping = {
   	version false
