c61dbee2688c38a77a4d13a0f5087f583a3ad3bf
mario martinez <amakenadog@gmail.com>
2014-09-04 08:49:14 -0600

    Agregado valores de presentaciones y sus medidas

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -16,19 +16,19 @@ grails.project.groupId = "ni.sb" // change this to alter the default package nam
 // The ACCEPT header will not be used for content negotiation for user agents containing the following strings (defaults to the 4 major rendering engines)
 grails.mime.disable.accept.header.userAgents = ['Gecko', 'WebKit', 'Presto', 'Trident']
 grails.mime.types = [ // the first one is the default format
-    all:           '*/*', // 'all' maps to '*' or the first available format in withFormat
-    atom:          'application/atom+xml',
-    css:           'text/css',
-    csv:           'text/csv',
-    form:          'application/x-www-form-urlencoded',
-    html:          ['text/html','application/xhtml+xml'],
-    js:            'text/javascript',
-    json:          ['application/json', 'text/json'],
-    multipartForm: 'multipart/form-data',
-    rss:           'application/rss+xml',
-    text:          'text/plain',
-    hal:           ['application/hal+json','application/hal+xml'],
-    xml:           ['text/xml', 'application/xml']
+  all:           '*/*', // 'all' maps to '*' or the first available format in withFormat
+  atom:          'application/atom+xml',
+  css:           'text/css',
+  csv:           'text/csv',
+  form:          'application/x-www-form-urlencoded',
+  html:          ['text/html','application/xhtml+xml'],
+  js:            'text/javascript',
+  json:          ['application/json', 'text/json'],
+  multipartForm: 'multipart/form-data',
+  rss:           'application/rss+xml',
+  text:          'text/plain',
+  hal:           ['application/hal+json','application/hal+xml'],
+  xml:           ['text/xml', 'application/xml']
 ]
 
 // URL Mapping Cache Max Size, defaults to 5000
@@ -47,20 +47,20 @@ grails.controllers.defaultScope = 'singleton'
 
 // GSP settings
 grails {
-    views {
-        gsp {
-            encoding = 'UTF-8'
-            htmlcodec = 'xml' // use xml escaping instead of HTML4 escaping
-            codecs {
-                expression = 'html' // escapes values inside ${}
-                scriptlet = 'html' // escapes output from scriptlets in GSPs
-                taglib = 'none' // escapes output from taglibs
-                staticparts = 'none' // escapes output from static template parts
-            }
-        }
-        // escapes all not-encoded output at final stage of outputting
-        // filteringCodecForContentType.'text/html' = 'html'
+  views {
+    gsp {
+      encoding = 'UTF-8'
+      htmlcodec = 'xml' // use xml escaping instead of HTML4 escaping
+      codecs {
+          expression = 'html' // escapes values inside ${}
+          scriptlet = 'html' // escapes output from scriptlets in GSPs
+          taglib = 'none' // escapes output from taglibs
+          staticparts = 'none' // escapes output from static template parts
+      }
     }
+    // escapes all not-encoded output at final stage of outputting
+    // filteringCodecForContentType.'text/html' = 'html'
+  }
 }
 
 
@@ -90,34 +90,34 @@ grails.hibernate.pass.readonly = false
 grails.hibernate.osiv.readonly = false
 
 environments {
-    development {
-        grails.logging.jul.usebridge = true
-    }
-    production {
-        grails.logging.jul.usebridge = false
-        // TODO: grails.serverURL = "http://www.changeme.com"
-    }
+  development {
+    grails.logging.jul.usebridge = true
+  }
+  production {
+    grails.logging.jul.usebridge = false
+    // TODO: grails.serverURL = "http://www.changeme.com"
+  }
 }
 
 // log4j configuration
 log4j = {
-    // Example of changing the log pattern for the default console appender:
-    //
-    //appenders {
-    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    //}
+  // Example of changing the log pattern for the default console appender:
+  //
+  //appenders {
+  //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
+  //}
 
-    error  'org.codehaus.groovy.grails.web.servlet',        // controllers
-           'org.codehaus.groovy.grails.web.pages',          // GSP
-           'org.codehaus.groovy.grails.web.sitemesh',       // layouts
-           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-           'org.codehaus.groovy.grails.web.mapping',        // URL mapping
-           'org.codehaus.groovy.grails.commons',            // core / classloading
-           'org.codehaus.groovy.grails.plugins',            // plugins
-           'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
-           'org.springframework',
-           'org.hibernate',
-           'net.sf.ehcache.hibernate'
+  error  'org.codehaus.groovy.grails.web.servlet',        // controllers
+         'org.codehaus.groovy.grails.web.pages',          // GSP
+         'org.codehaus.groovy.grails.web.sitemesh',       // layouts
+         'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+         'org.codehaus.groovy.grails.web.mapping',        // URL mapping
+         'org.codehaus.groovy.grails.commons',            // core / classloading
+         'org.codehaus.groovy.grails.plugins',            // plugins
+         'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
+         'org.springframework',
+         'org.hibernate',
+         'net.sf.ehcache.hibernate'
 }
 
 
@@ -139,3 +139,19 @@ grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 	'/**/favicon.ico':                ['permitAll']
 ]
 
+ni.sb.presentationsAndMeasures = [
+  Crema:
+    ["5g", "15g", "30g","50g"],
+  Talco:
+    ["80gm"],
+  Spray:
+    ["120ml", "150ml", "250ml", "356ml", "475ml", "159gm"],
+  Tableta:
+    ["2tabs", "4tabs", "6tabs", "8tabs", "10tabs", "24tabs", "50tabs", "60tabs", "100tabs", "300tabs"],
+  Soluble:
+    ["20g", "25g", "100g", "125g", "200g", "1kg"],
+  Suspencion:
+    ["120ml", "360ml"],
+  Inyectable:
+    ["10ml", "20ml", "25ml", "50ml", "100ml", "250ml", "500ml"]
+]
