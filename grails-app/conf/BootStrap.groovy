78880b18c457dc1a9f8138882ef020c6590d8d42
mario martinez <amakenadog@gmail.com>
2014-09-07 11:13:04 -0600

    Refactorizado datos de prueba

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -7,7 +7,6 @@ class BootStrap {
   	switch(Environment.current) {
   		case Environment.DEVELOPMENT:
         //PROVIDERS
-        /*
   			def provider1 = new Provider(
   				name:"provider1",
   				address:"address1",
@@ -16,48 +15,21 @@ class BootStrap {
 
         def product1 = new Product(name:"product1", code:"123")
 
-          def presentation1 = new Presentation(name:"Crema")
-
-            def m1 = new Measure(name:"5g")
-            def m2 = new Measure(name:"30g")
-
-            presentation1.addToMeasures m1
-            presentation1.addToMeasures m2
-
-          def presentation2 = new Presentation(name:"Talco")
-
-            def m3 = new Measure(name:"80mg")
-
-            presentation2.addToMeasures m3
+          def presentation1 = new Presentation(name:"Crema", measures:["15g", "30g"])
+          def presentation2 = new Presentation(name:"Talco", measures:["80gm"])
 
           product1.addToPresentations presentation1
           product1.addToPresentations presentation2
 
         def product2 = new Product(name:"product2", code:"456")
 
-          def presentation3 = new Presentation(name:"Soluble")
-
-            def m4 = new Measure(name:"25g")
-
-            presentation3.addToMeasures m4
-
+          def presentation3 = new Presentation(name:"Soluble", measures:["25g", "100g", "125g"])
           product2.addToPresentations presentation3
 
         def product3 = new Product(name:"product3", code:"789")
 
-          def presentation4 = new Presentation(name:"Spray")
-
-            def m5 = new Measure(name:"120ml")
-            def m6 = new Measure(name:"250ml")
-
-            presentation4.addToMeasures m5
-            presentation4.addToMeasures m6
-
-          def presentation5 = new Presentation(name:"Tableta")
-
-            def m7 = new Measure(name:"4tabs")
-
-            presentation5.addToMeasures m7
+          def presentation4 = new Presentation(name:"Spray", measures:["150ml", "356ml", "475ml", "159gm"])
+          def presentation5 = new Presentation(name:"Tableta", measures:["2tabs", "300tabs"])
 
           product3.addToPresentations presentation4
           product3.addToPresentations presentation5
@@ -73,7 +45,6 @@ class BootStrap {
             log.error "[$error.field: $error.defaultMessage]"
           }
         }
-        */
 
   			def provider2 = new Provider(
   				name:"provider2",
@@ -83,18 +54,12 @@ class BootStrap {
 
         def product4 = new Product(name:"product4", code:"10111")
 
-          def presentation6 = new Presentation(name:"Spray")
-
-            presentation6.addToMeasures(new Measure(name:"250ml")).addToMeasures(new Measure(name:"356ml"))
-
+          def presentation6 = new Presentation(name:"Spray", measures:["250ml", "356ml"])
           product4.addToPresentations presentation6
 
         def product5 = new Product(name:"product5", code:"45614")
 
-          def presentation7 = new Presentation(name:"Spray")
-
-            presentation7.addToMeasures(new Measure(name:"475ml"))
-
+          def presentation7 = new Presentation(name:"Spray", measures:["475ml"])
           product5.addToPresentations presentation7
 
         def providerTwoProducts = [product4, product5]
@@ -109,10 +74,9 @@ class BootStrap {
           }
         }
 
-  			assert Provider.count() == 1
-  			//assert provider1.products.size() == 3
+  			assert Provider.count() == 2
+  			assert provider1.products.size() == 3
   			assert provider2.products.size() == 2
-        //println provider2.products.presentations.measures
 
         //CLIENTS
         def client1 = new Client(fullName:"juan perez", address:"Address1", identificationCard:"291-290160-0001w", phones:["23114455", "88554477"])
