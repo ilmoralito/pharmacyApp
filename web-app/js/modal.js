1ac468db561cd525d24e38bd8acfe9fc40b578e1
Hagoromo <lumiesza@gmail.com>
2014-10-09 16:41:49 -0600

    agregando js para la ventana modal

---
+++ web-app/js/modal.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	$("a").click(function() {
+
+		var varId =  $(this).data("id");
+	    $("#idModal").html(varId);
+
+		var varInvoice =  $(this).data("invoice");
+	    $("#invoiceModal").html(varInvoice);
+
+	   	var varProvider =  $(this).data("provider");
+	    $("#providerModal").html(varProvider);
+
+		var varDutyDate =  $(this).data("dutyDate");
+	    $("#dutyDateModal").html(varDutyDate);
+
+	    var varBalance =  $(this).data("balance");
+	    $("#balanceModal").html(varBalance);
+
+	    var varTypeOfPurchase =  $(this).data("typeOfPurchase");
+	    $("#typeOfPurchaseModal").html(varTypeOfPurchase);
+
+	    var varStatus =  $(this).data("status");
+	    $("#statusModal").html(varStatus);
+
+	});
+});
