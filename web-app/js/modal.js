6951bd066043cca77cb21e5936218b7c82019376
Hagoromo <lumiesza@gmail.com>
2014-10-13 09:48:03 -0600

    terminando de agregar y modificar los elementos en el modal

--- web-app/js/modal.js
+++ web-app/js/modal.js
@@ -3,11 +3,15 @@ $(document).ready(function() {
 		e.preventDefault();
 		var data = $(this).data();
 		$("a#idModal").attr("href", "/pharmacyApp/purchaseOrder/show/" + data.id);
-	    $("#invoiceModal").html(data.invoice);
+	    $("#invoiceModal").html("Factura #" + data.invoice);
 	    $("#providerModal").html(data.provider);
+	    $("#payDateModal").html((data.paydata).substr(0,10));
 	    $("#dutyDateModal").html((data.date).substr(0,10));
 	    $("#balanceModal").html(data.balance);
 	    $("#typeOfPurchaseModal").html(data.type);
-	    $("#statusModal").html(data.status);
+	    if(data.status == true)
+	    	$("#statusModal").html("Cancelado");
+	    else
+	    	$("#statusModal").html("Pendiente");
 	});
 });
