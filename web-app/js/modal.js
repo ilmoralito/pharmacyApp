743579aca8666834db85b182f97f35846fb755f0
mario martinez <amakenadog@gmail.com>
2014-10-14 11:26:24 -0600

    Corregido direccion para editar orden

--- web-app/js/modal.js
+++ web-app/js/modal.js
@@ -2,16 +2,16 @@ $(document).ready(function() {
 	$("a#modalOrders").on("click", function(e) {
 		e.preventDefault();
 		var data = $(this).data();
-		$("a#idModal").attr("href", "/pharmacyApp/purchaseOrder/show/" + data.id);
-	    $("#invoiceModal").html("Factura #" + data.invoice);
-	    $("#providerModal").html(data.provider);
-	    $("#payDateModal").html((data.paydata).substr(0,10));
-	    $("#dutyDateModal").html((data.date).substr(0,10));
-	    $("#balanceModal").html(data.balance);
-	    $("#typeOfPurchaseModal").html(data.type);
-	    if(data.status == true)
-	    	$("#statusModal").html("Cancelado");
-	    else
-	    	$("#statusModal").html("Pendiente");
+		$("a#idModal").attr("href", "/pharmacyApp/purchaseOrder/edit/" + data.id);
+    $("#invoiceModal").html("Factura #" + data.invoice);
+    $("#providerModal").html(data.provider);
+    $("#payDateModal").html((data.paydata).substr(0,10));
+    $("#dutyDateModal").html((data.date).substr(0,10));
+    $("#balanceModal").html(data.balance);
+    $("#typeOfPurchaseModal").html(data.type);
+    if(data.status == true)
+    	$("#statusModal").html("Cancelado");
+    else
+    	$("#statusModal").html("Pendiente");
 	});
 });
