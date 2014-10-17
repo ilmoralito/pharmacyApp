b0150fb118c385f79b8982ac4f2a6ae29924cfc2
mario martinez <amakenadog@gmail.com>
2014-10-17 08:15:27 -0600

    Agregada propiedad nombre generico a entidad medicina

--- grails-app/domain/ni/sb/Medicine.groovy
+++ grails-app/domain/ni/sb/Medicine.groovy
@@ -2,9 +2,11 @@ package ni.sb
 
 class Medicine extends Product {
 	String code
+  String genericName
 
   static constraints = {
     code blank:false
+    genericName unique:true, nullable:true
     presentations nullable:false
   }
 
