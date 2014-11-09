2a8cff5f902d5ff945cc608c961faa755fcefee4
Hagoromo <lumiesza@gmail.com>
2014-11-09 10:03:25 -0600

    el ajax necesario para el nuevo cliente en ventas

--- web-app/js/adding.js
+++ web-app/js/adding.js
@@ -5,10 +5,9 @@ $(function(){
 		$("#box").toggle()
 	})
 
-	$("#send").on("click", function(e){
+	$("#sendProduct").on("click", function(e){
 		e.preventDefault();
-		var product = $("#newProduct"),
-				providerId = $("#providerId").val();
+		var product = $("#newProduct"), providerId = $("#providerId").val();
 
 		$.ajax({
 			url:"addProduct/",
@@ -22,4 +21,25 @@ $(function(){
 
 		product.val("");
 	})
+
+
+	$("#sendClient").on("click", function(e){
+		e.preventDefault();
+		var fullName = $("#fullName").val();
+		var address = $("#address").val();
+		var identificationCard = $("#identificationCard").val();
+		$.ajax({
+			url:"addClient/",
+			data:{fullName:fullName, address:address, identificationCard:identificationCard},
+			success:function(data) {
+				var opt = $("<option>", { value:data.id, text:data.name, selected:true });
+
+				$("#client.id").append(opt);
+			}
+		})
+		fullName.val("");
+		address.val("");
+		identificationCard.val("");
+	})
+
 });
