524f3acadf8d1b2b9b9f55af5b5858a177d5725a
Hagoromo <lumiesza@gmail.com>
2014-12-09 16:30:48 -0600

    añadiendo localStorage para recordar si dejan abiertas o no las opciones de Filtrar

--- web-app/js/saleFilter.js
+++ web-app/js/saleFilter.js
@@ -13,4 +13,16 @@ $(document).ready(function() {
 		allSelectedText: "Todos seleccionados",
 		numberDisplayed: 3
 	});
+
+	if (localStorage.Filter == "true") {
+	$("#filterSales").show()
+	} else {
+		$("#filterSales").hide()
+	}
+
+	$("#filterSale").on("click", function() {
+	var target = $("#filterSales")
+	target.toggle()
+	localStorage.Filter = target.is(':visible')
+	})
 });
