852b58dd0acf658b809fc2ecb2b0b7b3eced6399
Mario Martinez <amakenadog@gmail.com>
2016-04-07 13:13:27 -0600

    Update measures or details when change presentation/brand select box

--- web-app/js/purchase-order.js
+++ web-app/js/purchase-order.js
@@ -1,16 +1,14 @@
 $('form').find("select:first").on('change', function() {
     var $this = $(this);
-    var data = JSON.parse(JSON.stringify($this.find(':selected').data('info')));
+    var optionSelected = $("option:selected", this);
+    var info = optionSelected.data("info")
     var target = $this.parent().parent().next().find('select');
 
     target.find('option').remove()
-    console.log(data);
 
-    for (var i = data.length - 1; i >= 0; i--) {
-        /*
-        var option = $('<option>', { value: data[i].id}, text: data[i].name });
+    for (var i = info.length - 1; i >= 0; i--) {
+        var option = $('<option>', { value: info[i].id, text: info[i].name });
 
         target.append(option);
-        */
     }
 });
